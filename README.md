3d2254e7ecc24a605daf6ad81a4a323b6944512b
courtbfh <entreprisedeladds@gmail.com>
2026-05-01 00:32:50 +0200

    update readme

---
+++ README.md
@@ -0,0 +1,5 @@
+### Magneto : A Magnetic Field Simulator
+
+Magneto is a magnetic field simulator, totaly wwrited in C++, enabling people to understand and make some experiment/ measurments with magnet! 
+
+![Static Magnetic Field](image/magnetic_static.png)
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/ordi2430538/Documents/coding_project_flavortown/magnetic-sim
+# Install script for directory: C:/Users/ordi2430538/Documents/coding_project_flavortown/Magnetic-Sim
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/ordi2430538/Documents/coding_project_flavortown/magnetic-sim/build/install_local_manifest.txt"
+  file(WRITE "C:/Users/ordi2430538/Documents/coding_project_flavortown/Magnetic-Sim/build/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "C:/Users/ordi2430538/Documents/coding_project_flavortown/magnetic-sim/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/ordi2430538/Documents/coding_project_flavortown/Magnetic-Sim/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
